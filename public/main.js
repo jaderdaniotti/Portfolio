e29aecce8b08c538463ff505ae89d42395def600
Jader Daniotti <jaderdaniotti1999@gmail.com>
2025-03-07 01:12:14 +0100

    modificare media query ipad

--- public/main.js
+++ public/main.js
@@ -1,11 +1,56 @@
 console.log('Hello World!');
-window.addEventListener('scroll', function() {
+
+window.addEventListener('load', function(){
+  const load = document.querySelector('.sectionabout1');
+  load.classList.add('show');
+  window.addEventListener('scroll', function() {
     console.log(window.scrollY);
     var navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) { // Regola questo valore per cambiare quando la navbar si rimpicciolisce
+    if (window.scrollY ) { // Regola questo valore per cambiare quando la navbar si rimpicciolisce
       navbar.classList.add('navbar-shrink');
     } else {
       navbar.classList.remove('navbar-shrink');
     }
   });
+})
+  // Navbar
+
   
+  //section 2
+  document.addEventListener('DOMContentLoaded', function() {
+    const quote = document.querySelector('.sectionabout2');
+  
+    function checkSlide() {
+      // Calcola la posizione di attivazione a 5 pixel dall'inizio del div
+      const slideInAt = quote.offsetTop - (window.innerHeight);
+      const quoteBottom = quote.offsetTop + quote.offsetHeight;
+      const isHalfShown = window.scrollY > slideInAt;
+      const isNotScrolledPast = window.scrollY < quoteBottom;
+  
+      if (isHalfShown && isNotScrolledPast) {
+        quote.classList.add('show');
+      }
+    }
+  
+    window.addEventListener('scroll', checkSlide);
+  });
+
+  //section 3
+  document.addEventListener('DOMContentLoaded', function() {
+    const quote = document.querySelector('.sectionabout3');
+  
+    function checkSlide() {
+      // Calcola la posizione di attivazione a 5 pixel dall'inizio del div
+      const slideInAt = quote.offsetTop - (window.innerHeight - 5);
+      const quoteBottom = quote.offsetTop + quote.offsetHeight;
+      const isHalfShown = window.scrollY > slideInAt;
+      const isNotScrolledPast = window.scrollY < quoteBottom;
+  
+      if (isHalfShown && isNotScrolledPast) {
+        quote.classList.add('show');
+      }
+    }
+  
+    window.addEventListener('scroll', checkSlide);
+  });
+
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,87 +1,71 @@
 <x-componenti>
-    <div class="row my-2 py-1 border-top border-bottom">
-        <div class="col-12 col-lg-4 d-flex justify-content-center align-items-center">
-            <img src="images\shangai.jpg" alt="" class="img-fluid">
-        </div>
-        <div class="col-12 col-lg-8 align-self-center py-4 div-description">
-            <h2><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
-              </svg> Chi sono <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
-              </svg></h2>
-            <p class="description">Nato il 18 dicembre 1999, il mio percorso professionale ha avuto inizio all'età di diciotto anni, dopo aver completato la mia formazione come elettricista. Spinto dal desiderio di crescita personale e professionale, ho intrapreso un'esperienza all'estero, trasferendomi in Francia per imparare una nuova lingua e immergermi in una cultura diversa.
-                <br>
-                I primi mesi in Francia sono stati caratterizzati da un intenso contatto con il pubblico, lavorando nei mercati e imparando a comunicare nonostante le barriere linguistiche. Successivamente, ho lavorato nel settore edile, collaborando con un team multiculturale e sviluppando la capacità di adattarmi a contesti lavorativi diversi.
-                <br>
-                Nel corso degli anni, ho svolto diverse mansioni, ritornando infine al mio mestiere di elettricista. Questa esperienza mi ha permesso di consolidare le mie competenze tecniche, acquisire una nuova lingua, sviluppare l'autonomia personale e imparare a interagire efficacemente con persone di diverse culture.
-                <br>
-                La mia passione per i viaggi mi ha portato a esplorare numerose città e paesi, arricchendo il mio bagaglio culturale e personale. Durante un viaggio a Shanghai, ho incontrato un programmatore che ha acceso la mia curiosità per il mondo della programmazione, mostrandomi una prospettiva lavorativa innovativa e stimolante.
-                <br>
-                Al mio rientro in Italia, ho iniziato a esplorare autonomamente il mondo della programmazione, utilizzando app per creare un primo sito di eCommerce. Contemporaneamente, ho lavorato come elettrauto presso una ditta specializzata in allestimenti per furgoni, mantenendo un legame con il settore elettrico.
-                <br>
-                Spinto dalla passione per la programmazione, ho intrapreso un percorso di autoapprendimento, utilizzando risorse online come tutorial su YouTube. Nell'ottobre 2024, ho deciso di iscrivermi al corso Full Stack Developer presso Aulab, segnando un passaggio formale e strutturato nel mondo della programmazione.
-                <br>
-                Dopo un mese dall'inizio del corso, ho preso la decisione di dedicarmi completamente alla programmazione, lasciando il mio impiego per concentrarmi a tempo pieno sullo studio e la crescita professionale nel settore IT.
-                <br>
-                Il mio percorso professionale è caratterizzato da una forte motivazione all'apprendimento continuo, dalla capacità di adattamento a contesti diversi e dalla passione per la scoperta di nuove sfide. Sono determinato a mettere a frutto le mie competenze e la mia esperienza per intraprendere una carriera di successo nel mondo della programmazione.
-                </p>
-        </div>
-    </div>
-    <div class="row my-3 bg-dark text-light rounded">
-        <div class="col-12 col-lg-8 align-self-center py-4 div-description ">
-            <h2>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                </svg> Cosa cerco <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                </svg>
-            </h2>
-            <p class="description">Il mio obiettivo è trovare un'opportunità nel mondo dello sviluppo web o di app, in un'azienda che valorizzi il potenziale di un apprendista motivato e desideroso di crescere. Sono alla ricerca di un ambiente di lavoro stimolante, dove poter mettere in pratica le competenze acquisite e continuare ad imparare dai professionisti del settore. Desidero contribuire a progetti che abbiano un impatto positivo, con un focus su tematiche umanistiche e sociali. Sono consapevole della mia limitata esperienza, ma sono pronto a dedicare tempo ed energie per acquisire nuove conoscenze e diventare un membro prezioso del team. Cerco un'azienda che creda nel valore dell'investimento sulle persone, offrendo un percorso di crescita strutturato e la possibilità di lavorare su progetti innovativi e significativi. La mia passione per la programmazione, unita alla mia esperienza di vita e alla mia capacità di adattamento, mi rendono un candidato ideale per un'azienda che cerca talenti emergenti con una forte motivazione e un approccio proattivo.
-                <br>
-                I primi mesi in Francia sono stati caratterizzati da un intenso contatto con il pubblico, lavorando nei mercati e imparando a comunicare nonostante le barriere linguistiche. Successivamente, ho lavorato nel settore edile, collaborando con un team multiculturale e sviluppando la capacità di adattarmi a contesti lavorativi diversi.
-                <br>
-                Nel corso degli anni, ho svolto diverse mansioni, ritornando infine al mio mestiere di elettricista. Questa esperienza mi ha permesso di consolidare le mie competenze tecniche, acquisire una nuova lingua, sviluppare l'autonomia personale e imparare a interagire efficacemente con persone di diverse culture.
-                <br>
-                La mia passione per i viaggi mi ha portato a esplorare numerose città e paesi, arricchendo il mio bagaglio culturale e personale. Durante un viaggio a Shanghai, ho incontrato un programmatore che ha acceso la mia curiosità per il mondo della programmazione, mostrandomi una prospettiva lavorativa innovativa e stimolante.
-                <br>
-                Al mio rientro in Italia, ho iniziato a esplorare autonomamente il mondo della programmazione, utilizzando app per creare un primo sito di eCommerce. Contemporaneamente, ho lavorato come elettrauto presso una ditta specializzata in allestimenti per furgoni, mantenendo un legame con il settore elettrico.
-                <br>
-                Spinto dalla passione per la programmazione, ho intrapreso un percorso di autoapprendimento, utilizzando risorse online come tutorial su YouTube. Nell'ottobre 2024, ho deciso di iscrivermi al corso Full Stack Developer presso Aulab, segnando un passaggio formale e strutturato nel mondo della programmazione.
-                <br>
-                Dopo un mese dall'inizio del corso, ho preso la decisione di dedicarmi completamente alla programmazione, lasciando il mio impiego per concentrarmi a tempo pieno sullo studio e la crescita professionale nel settore IT.
-                <br>
-                Il mio percorso professionale è caratterizzato da una forte motivazione all'apprendimento continuo, dalla capacità di adattamento a contesti diversi e dalla passione per la scoperta di nuove sfide. Sono determinato a mettere a frutto le mie competenze e la mia esperienza per intraprendere una carriera di successo nel mondo della programmazione.
-                </p>
-        </div>
-        <div class="col-12 col-lg-4 d-flex justify-content-center align-items-center">
-            <img src="images\code1.jpg" alt="" class="img-fluid">
-        </div>
-    </div>
-    <div class="container bg-img-about-contact">
-        <div class="row descriptionContactJS">
-            <div class="col-12 mt-2 mb-5">
-                <button class="btnContact">
-                    <a href="{{route('contact')}}">
-                        Se il mio profilo ti interessa cosa aspetti? <br>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-down-right" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0z"/>
-                        </svg>
-                        Contattami!
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-down-left" viewBox="0 0 16 16">
-                              <path fill-rule="evenodd" d="M2 13.5a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 0-1H3.707L13.854 2.854a.5.5 0 0 0-.708-.708L3 12.293V7.5a.5.5 0 0 0-1 0z"/>
-                        </svg><br>Vai alla sezione contatti
-                    </a>
-                </button>
-            </div>      
-        </div>
+    
+    <div class="row my-2 py-1  sectionabout1">
+      <div class="col-12 col-lg-4 d-flex justify-content-center align-items-center">
+        <img src="images\shangai.jpg" alt="" class="img-fluid">
+      </div>
+      <div class="col-12 col-lg-8 align-self-center py-4 div-description">
+        <h2 class="quote">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+          </svg> Chi sono <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+          </svg>
+        </h2>
+        <p class="description">Nato il 18 dicembre 1999, il mio percorso professionale ha avuto inizio all'età di diciotto anni, dopo aver completato la mia formazione come elettricista. <br> Spinto dal desiderio di crescita personale e professionale, ho intrapreso un'esperienza all'estero, trasferendomi in Francia per imparare una nuova lingua e immergermi in una cultura diversa. <br> I primi mesi in Francia sono stati caratterizzati da un intenso contatto con il pubblico, lavorando nei mercati e imparando a comunicare nonostante le barriere linguistiche. <br> Successivamente, ho lavorato nel settore edile, collaborando con un team multiculturale e sviluppando la capacità di adattarmi a contesti lavorativi diversi. <br> Nel corso degli anni, ho svolto diverse mansioni, ritornando infine al mio mestiere di elettricista. Questa esperienza mi ha permesso di consolidare le mie competenze tecniche, acquisire una nuova lingua, sviluppare l'autonomia personale e imparare a interagire efficacemente con persone di diverse culture. <br>
+        </p>
+      </div>
     </div>
 
-</x-componenti>
-Se il mio profilo ti interessa cosa aspetti? <br>
-<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-down-right" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0z"/>
-</svg>
-Contattami!
-<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-down-left" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M2 13.5a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 0-1H3.707L13.854 2.854a.5.5 0 0 0-.708-.708L3 12.293V7.5a.5.5 0 0 0-1 0z"/>
-</svg>
+    <div class="row my-2 py-1  sectionabout2">
+      <div class="col-12 col-lg-8 align-self-center py-4 div-description">
+        <h2>
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-signpost-2" viewBox="0 0 16 16">
+            <path d="M7 1.414V2H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h5v1H2.5a1 1 0 0 0-.8.4L.725 8.7a.5.5 0 0 0 0 .6l.975 1.3a1 1 0 0 0 .8.4H7v5h2v-5h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H9V6h4.5a1 1 0 0 0 .8-.4l.975-1.3a.5.5 0 0 0 0-.6L14.3 2.4a1 1 0 0 0-.8-.4H9v-.586a1 1 0 0 0-2 0M13.5 3l.75 1-.75 1H2V3zm.5 5v2H2.5l-.75-1 .75-1z" />
+          </svg>
+          </svg> Percorso <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-signpost-2" viewBox="0 0 16 16">
+            <path d="M7 1.414V2H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h5v1H2.5a1 1 0 0 0-.8.4L.725 8.7a.5.5 0 0 0 0 .6l.975 1.3a1 1 0 0 0 .8.4H7v5h2v-5h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H9V6h4.5a1 1 0 0 0 .8-.4l.975-1.3a.5.5 0 0 0 0-.6L14.3 2.4a1 1 0 0 0-.8-.4H9v-.586a1 1 0 0 0-2 0M13.5 3l.75 1-.75 1H2V3zm.5 5v2H2.5l-.75-1 .75-1z" />
+          </svg>
+          </svg>
+        </h2>
+        <p class="description"> La mia passione per i viaggi mi ha portato a esplorare numerose città e paesi, arricchendo il mio bagaglio culturale e personale. Durante un viaggio a Shanghai, ho incontrato un programmatore che ha acceso la mia curiosità per il mondo della programmazione, mostrandomi una prospettiva lavorativa innovativa e stimolante. <br> Al mio rientro in Italia, ho iniziato a esplorare autonomamente il mondo della programmazione, utilizzando app per creare un primo sito di eCommerce. Contemporaneamente, ho lavorato come elettrauto presso una ditta specializzata in allestimenti per furgoni, mantenendo un legame con il settore elettrico. <br> Spinto dalla passione per la programmazione, ho intrapreso un percorso di autoapprendimento, utilizzando risorse online come tutorial su YouTube. Nell'ottobre 2024, ho deciso di iscrivermi al corso Full Stack Developer presso Aulab, segnando un passaggio formale e strutturato nel mondo della programmazione. <br> Dopo un mese dall'inizio del corso, ho preso la decisione di dedicarmi completamente alla programmazione, lasciando il mio impiego per concentrarmi a tempo pieno sullo studio e la crescita professionale nel settore IT. </p>
+      </div>
+      <div class="col-12 col-lg-4 d-flex justify-content-center align-items-center">
+        <img src="images\city.jpg" alt="" class="img-fluid">
+      </div>
+    </div>
+
+    <div class="row my-3 rounded sectionabout3">
+      <div class="col-12 col-lg-4 d-flex justify-content-center align-items-center">
+        <img src="images\code1.jpg" alt="" class="img-fluid">
+      </div>
+      <div class="col-12 col-lg-8 align-self-center py-4 div-description ">
+        <h2>
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+          </svg> Cosa cerco <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+          </svg>
+        </h2>
+        <p class="description">Il mio obiettivo è trovare un'opportunità nel mondo dello sviluppo web o di app, in un'azienda che valorizzi il potenziale di un apprendista motivato e desideroso di crescere. Sono alla ricerca di un ambiente di lavoro stimolante, dove poter mettere in pratica le competenze acquisite e continuare ad imparare dai professionisti del settore. Desidero contribuire a progetti che abbiano un impatto positivo, con un focus su tematiche umanistiche e sociali. Sono consapevole della mia limitata esperienza, ma sono pronto a dedicare tempo ed energie per acquisire nuove conoscenze e diventare un membro prezioso del team. Cerco un'azienda che creda nel valore dell'investimento sulle persone, offrendo un percorso di crescita strutturato e la possibilità di lavorare su progetti innovativi e significativi. La mia passione per la programmazione, unita alla mia esperienza di vita e alla mia capacità di adattamento, mi rendono un candidato ideale per un'azienda che cerca talenti emergenti con una forte motivazione e un approccio proattivo. Sono convinto che la mia determinazione e la mia passione per la programmazione mi permetteranno di raggiungere i miei obiettivi professionali e di contribuire al successo dell'azienda che mi offrirà un'opportunità di crescita e sviluppo. </p>
+      </div>
+    </div>
+
+    <div class="container bg-img-about-contact">
+      <div class="row descriptionContactJS">
+        <div class="col-12 mt-2 mb-5">
+            <a href="{{route('contact')}}">
+                <button class="btnContact">
+                    Se il mio profilo ti interessa cosa aspetti? <br>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-down-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0z" />
+                    </svg> Contattami! <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-down-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2 13.5a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 0-1H3.707L13.854 2.854a.5.5 0 0 0-.708-.708L3 12.293V7.5a.5.5 0 0 0-1 0z" />
+                    </svg>
+                    <br>Vai alla sezione contatti 
+                </button>
+            </a>
+        </div>
+      </div>
+    </div>
+  </x-componenti>
